f8049595f6c68566bb09535b26b876f9d70ae759
VirgioAryoPorwondaru <ndarundaru9@gmail.com>
2022-06-13 16:37:44 +0700

    Add Jumping script
    - add jumping script
    - using raycast and layers

--- Assets/CharacterMoveController.cs
+++ Assets/CharacterMoveController.cs
@@ -9,21 +9,65 @@ public class CharacterMoveController : MonoBehaviour
     public float maxSpeed;
     private Rigidbody2D rig;
 
+    [Header("Jump")]
+    public float jumpAccel;
+    private bool isJumping;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+    private bool isOnGround;
+
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
     }
-    void FixedUpdate()
+
+    void Update()
     {
+        // read input
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+            }
+        }
+    }
+
+   
+    private void FixedUpdate()
+    {
+        // raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,
+        groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+        // calculate velocity vector
         Vector2 velocityVector = rig.velocity;
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
         velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime,
         0.0f, maxSpeed);
         rig.velocity = velocityVector;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down *
+        groundRaycastDistance), Color.white);
     }
+
 }
+
