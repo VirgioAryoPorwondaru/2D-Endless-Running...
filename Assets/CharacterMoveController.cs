4487809989a403dbca0bc76805335c3a8cd954e0
VirgioAryoPorwondaru <ndarundaru9@gmail.com>
2022-06-13 15:41:01 +0700

    Moving Character
    - add script to character moving
    - add physics material to reduce friction

---
+++ Assets/CharacterMoveController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+    private Rigidbody2D rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        Vector2 velocityVector = rig.velocity;
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime,
+        0.0f, maxSpeed);
+        rig.velocity = velocityVector;
+    }
+    void Update()
+    {
+    }
+}
+
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
